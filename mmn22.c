9f35933281de83727a028f2154fd67d4fd4aaeb8
yonivr <33428475+yonivr@users.noreply.github.com>
2017-11-06 19:45:15 +0200

    Update mmn22.c

--- mmn22.c
+++ mmn22.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+zero a char array 
+*/
 void zeroarr(char s[])
 {
     int i;
@@ -11,51 +14,64 @@ void zeroarr(char s[])
     }
 }
 
-char *  splitComma(char s[])
+/**
+ * input  - char array s
+ * output - first string until comma
+ * purpose - return first word until comma
+ **/
+char *  splitComma(char s[],char c)
 {
     int i;//s iterator
     int j=0;//temp iterator
-    int gotComma=0;
-    char *res = malloc (sizeof (char) * strlen(s));
-    for(i=0;i<strlen(s);i++)
+    char *res = malloc (sizeof (char) * strlen(s)); //allocate a string to return
+    for(i=0;i<strlen(s);i++)//iterate until the end of s string
     {
-        if(s[i]==',')
+        if(s[i]==c)//in case comma is found return array until comma
         {
-            gotComma=1;
             return res;
         }
-        if(gotComma==0)
-        {
-            res[j]=s[i];
-            j++;
+        else
+        {//copy current char to res array if char is not comma and iterate res
+            if(s[i]!=' ')
+            {
+                res[j]=s[i];
+                j++;    
+            }
+            
         }   
     }
-    return res;
+    return res;//case no comma return the entire string
 }
-
+/**
+ * iterate s string , split string by comma and perform actions by the command
+ **/
 void iterateLine(char s[])
 {
-    int	i=-1, count = 0;
-    char c=',';
-    char *res = malloc (sizeof (char) * strlen(s));
-    res = splitComma(s);
-	printf("%s\n",res);
-	while(s[++i])
+    int	i=-1;
+    char c=',';//seperator 
+    char *res = malloc (sizeof (char) * strlen(s)); //allocate string for current word
+    res = splitComma(s,' ');//split string by space to find first command
+    printf("%s\n",res);
+    i=strlen(res);//start from next word after command
+    zeroarr(res);//zero res arr
+   res = splitComma(s+i,c);//find next word before comma
+   printf("%s\n",res);
+	while(s[++i])//iterate string s until the end
 	{
-	   if(s[i] == c) 
+	   if(s[i] == c) //case found seperator
 	   {
-	       zeroarr(res);
-	       res = splitComma(s+i+1);
+	       zeroarr(res);//zero the current word
+	       res = splitComma(s+i+1,c);//current word is set by offset i from start and comma seperator
 	       printf("%s\n",res);
-	       count++;
 	   }
 	}  
-	printf("%d\n",count);
+	
 }
 
 int main()
 {
-    char s[]="apple,banana,mango,melon";
+    //char s[]="apple    ,banana,mango,melon";
+    char s[]="read_comp A, 45.1, 23.7";
     iterateLine(s);
     return 0;
 }
